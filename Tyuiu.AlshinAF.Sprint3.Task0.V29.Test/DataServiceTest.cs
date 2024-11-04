ff7234f9014b3b60338a81310b7ee5db64778c74
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-04 23:04:54 +0500

    Add project files.

---
+++ Tyuiu.AlshinAF.Sprint3.Task0.V29.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.AlshinAF.Sprint3.Task0.V29.Lib;
+namespace Tyuiu.AlshinAF.Sprint3.Task0.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 10;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 9.946;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AlshinAF.Sprint3.Task0.V29/Program.cs
@@ -0,0 +1,3 @@
+﻿using Tyuiu.AlshinAF.Sprint3.Task0.V29.Lib;
+DataService ds = new DataService();
+Console.WriteLine(ds.GetSumSeries(0.5, 1, 10));
