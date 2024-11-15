0dcb4a38bb76bb67246e88a297dfb058a2eb00d1
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-11-15 23:47:41 +0500

    Task5

---
+++ Tyuiu.AlshinAF.Sprint3.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.AlshinAF.Sprint3.Task5.V6.Lib;
+namespace Tyuiu.AlshinAF.Sprint3.Task5.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 5;
+            int start1 = 1;
+            int start2 = 1;
+            int stop1 = 5;
+            int stop2 = 10;
+            double res = ds.GetSumSumSeries(start1, start2, stop1, stop2);
+            double wait = 40.917;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
